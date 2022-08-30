a13ac0ffd9c1e4ac010c6a60c20b380e4c82185f
JccastilloDev <juankarlitos22@hotmail.com>
2022-08-30 16:18:42 -0500

    ajuste package json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const dotenv = require('dotenv');
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes.js');
-
+dotenv.config();
 const app = express();
+
              // Si no hay var de entorno aplica puerto 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json()); // Middleware global
 app.use('/api/v1/workouts', v1WorkoutRouter );
